4c9862cf4074a191836bcaad64e315cb91fc4a2a
JoonasHohtari <zzounass@gmail.com>
2023-10-12 21:01:37 +0300

    Some advanced made in the course.

---
+++ CS/basics/DoWhileGame/Program.cs
@@ -0,0 +1,28 @@
+﻿Random attack = new Random();
+int damage = 0;
+int monsterHealth = 10;
+int heroHealth = 10;
+
+do
+{
+    Console.Write($"Hero attacks monster and deals {damage = attack.Next(1, 11)} damage. ");
+    monsterHealth -= damage;
+    Console.WriteLine($"Monster has {monsterHealth} health left.");
+
+    if (monsterHealth <= 0)
+    {
+        Console.WriteLine("You have defeated the monster!");
+        break;
+    }
+
+    Console.Write($"Monster attacks hero and deals {damage = attack.Next(1, 11)} damage. ");
+    heroHealth -= damage;
+    Console.WriteLine($"Hero has {heroHealth} health left.");
+
+    if (heroHealth <= 0)
+    {
+        Console.WriteLine("You have been defeated!");
+        break;
+    }
+
+} while (monsterHealth > 0 && heroHealth > 0);
---
+++ CS/basics/DoWhileOrWhile/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+Console.WriteLine("Enter a integer number between 5-10: ");
+
+int number = 0;
+bool isNumber = false;
+string? userInput;
+
+do
+{
+    userInput = Console.ReadLine();
+    isNumber = int.TryParse(userInput, out number);
+    if (!isNumber)
+    {
+        Console.WriteLine("You need to enter a number, try again: ");
+        continue;
+    }
+    else
+    {
+        if (number < 5 || number > 10)
+        {
+            Console.WriteLine("The number is not between 5-10, try again: ");
+        }
+    }
+
+} while (number < 5 || number > 10);
+
+Console.WriteLine("Your number is: " + number + ", You shall pass.");
+
+
+Console.WriteLine("Are you: Administrator, Manager or User?");
+
+string? userInput = null;
+
+while (userInput == null)
+{
+    userInput = Console.ReadLine();
+    if (userInput != null)
+    {
+        userInput = userInput.ToLower();
+        userInput = userInput.Trim();
+        if (userInput == "administrator" || userInput == "manager" || userInput == "user")
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("You need to enter: Adminstrator, Manager or User");
+            userInput = null;
+        }
+    }
+}
+
+Console.WriteLine("You are: " + userInput + ", You shall pass.");
+
+string[] myString = new string[2] { "I like pizza. I like roast chicken. I like salad.", "I like all three of the menu choices" };
+
+for (int i = 0; i < myString.Length; i++)
+{
+    int periodLocation = myString[i].IndexOf('.');
+    while (periodLocation != -1)
+    {
+        string sentence = myString[i].Substring(0, periodLocation);
+
+        sentence = sentence.Insert(periodLocation, "\n");
+        myString[i] = myString[i].Remove(0, periodLocation + 1).TrimStart();
+
+        Console.Write(sentence);
+
+        periodLocation = myString[i].IndexOf('.');
+    }
+    Console.Write(myString[i].TrimStart());
+}
+*/
